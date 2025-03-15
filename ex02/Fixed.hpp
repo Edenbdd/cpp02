cdc010e8c22180e7890913a4667dc946e73a2990
Edenbdd <aubertra@student.42.fr>
2025-03-15 16:36:14 +0100

    ready to set as finished

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: aubertra <aubertra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 14:39:48 by aubertra          #+#    #+#             */
-/*   Updated: 2025/03/15 15:55:07 by aubertra         ###   ########.fr       */
+/*   Updated: 2025/03/15 16:18:45 by aubertra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,5 @@ class Fixed
         static const int	_bits = 8;
 };
 
-//add surchage operateur << AVANT DE PRINT QUOIQUE CE SOIT
 std::ostream    &operator<<(std::ostream &o, Fixed const &to_print);
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aubertra <aubertra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 14:38:18 by aubertra          #+#    #+#             */
-/*   Updated: 2025/03/15 15:57:39 by aubertra         ###   ########.fr       */
+/*   Updated: 2025/03/15 16:35:51 by aubertra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int main(void)
 {
     {
-    std::cout << "Tests from subject: " << std::endl;
+    std::cout << "Tests from subject (++, ++, +, >, <, max) " << std::endl;
     Fixed a;
     Fixed const b( Fixed(5.05f) * Fixed(2) );
     std::cout << a << std::endl;
@@ -29,18 +29,29 @@ int main(void)
     std::cout << Fixed::max(a, b) << std::endl;
     }
     {
-    // std::cout << "Additional Tests: " << std::endl;
-    // Fixed c;
-    // Fixed const d( Fixed(5.05f) * Fixed(2) );
-    // std::cout << c << std::endl;
-    // std::cout << --c << std::endl;
-    // std::cout << c << std::endl;
-    // std::cout << c-- << std::endl; 
-    // std::cout << c << std::endl;
-
-    // std::cout << d << std::endl;
-
-    // std::cout << Fixed::min(c, d) << std::endl;
+    std::cout << "Additional Tests (--, --, -, <, >, min): " << std::endl;
+    Fixed a;
+    Fixed const b( Fixed(5.05f) * Fixed(2) );
+    std::cout << "a : " << a << std::endl;
+    std::cout << "--a : " << --a << std::endl;
+    std::cout << "a : " << a << std::endl;
+    std::cout << "a-- : " << a-- << std::endl;
+    std::cout << "a : " << a << std::endl; 
+    std::cout << "b : " << b << std::endl;
+    std::cout << "min(a, b) : " << Fixed::min(a, b) << std::endl;
+    }
+    {
+    std::cout << "Additional Tests (*, /, >=, ==, !=, <=): " << std::endl;
+    Fixed a(Fixed(5.05f) / Fixed(2));
+    Fixed const b( Fixed(5.05f) * Fixed(2));
+    std::cout << "a : " << a << std::endl;
+    std::cout << "b : " << b << std::endl;
+    std::cout << "a * b : " << a * b << std::endl;
+    std::cout << "a / b : " << a / b << std::endl;
+    std::cout << "(a != b) : " << (a != b) << std::endl;
+    std::cout << "(a == b) : " << (a == b) << std::endl;
+    std::cout << "(a <= b) : " << (a <= b) << std::endl;
+    std::cout << "(a >= b) : " << (a >= b) << std::endl;
     }
     return (0);
 }
